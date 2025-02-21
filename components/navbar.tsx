e3aea04b243794503e92ad997a4333f3507ff401
itork <itork555@gmail.com>
2025-02-21 22:49:54 +0100

    refactor: standardize quotes and improve Navbar component structure

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,10 @@
-"use client";
+'use client';
 
-import * as React from "react";
-import Link from "next/link";
-import Image from "next/image";
+import { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
-import { cn } from "@/lib/utils";
-//import { Icons } from '@/components/icons';
+import { cn } from '@/lib/utils';
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -14,7 +13,8 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
   navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu";
+} from '@/components/ui/navigation-menu';
+import React from 'react';
 
 const components: {
   title: string;
@@ -23,88 +23,89 @@ const components: {
   pro: boolean;
 }[] = [
   {
-    title: "Favorite Genres",
-    href: "/docs/primitives/alert-dialog",
+    title: 'Favorite Genres',
+    href: '/docs/primitives/alert-dialog',
     description:
-      "Discover your top genres and find more tracks that match your vibe.",
+      'Discover your top genres and find more tracks that match your vibe.',
     pro: false,
   },
   {
-    title: "Charts",
-    href: "/docs/primitives/hover-card",
+    title: 'Charts',
+    href: '/docs/primitives/hover-card',
     description:
-      "Explore your weekly vibe – see how your music shifts from day to day.",
+      'Explore your weekly vibe – see how your music shifts from day to day.',
     pro: false,
   },
   {
-    title: "Listening Time",
-    href: "/docs/primitives/progress",
+    title: 'Listening Time',
+    href: '/docs/primitives/progress',
     description:
-      "Track your total listening time and see your most dedicated music moments.",
+      'Track your total listening time and see your most dedicated music moments.',
     pro: false,
   },
   {
-    title: "Favorite Songs",
-    href: "/docs/primitives/scroll-area",
+    title: 'Favorite Songs',
+    href: '/docs/primitives/scroll-area',
     description:
       "Discover your all-time favorite songs, the ones you can't stop playing!",
     pro: false,
   },
   {
-    title: "Monthly Wrapped",
-    href: "/docs/primitives/tabs",
+    title: 'Monthly Wrapped',
+    href: '/docs/primitives/tabs',
     description:
-      "Relive your month in music—your top tracks and artists all in one place!",
+      'Relive your month in music—your top tracks and artists all in one place!',
     pro: false,
   },
   {
-    title: "AI Recomendation",
-    href: "/docs/primitives/tooltip",
+    title: 'AI Recomendation',
+    href: '/docs/primitives/tooltip',
     description:
-      "Get personalized music suggestions based on your unique taste.",
+      'Get personalized music suggestions based on your unique taste.',
     pro: true,
   },
 ];
 
 export function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 right-0 bg-background z-50 flex items-center px-4 py-2">
-      <div className="flex items-center space-x-4">
-        <Link href="/">
-          <Image src="/tune_stats_logo.png" alt="Logo" width={40} height={40} />
+    <nav className='fixed top-0 left-0 right-0 bg-background z-50 flex items-center px-4 py-2'>
+      <div className='flex items-center space-x-4'>
+        <Link href='/'>
+          <Image src='/tune_stats_logo.png' alt='Logo' width={40} height={40} />
         </Link>
       </div>
       <NavigationMenu>
-        {" "}
-        <NavigationMenuList className="ml-4">
+        <NavigationMenuList className='ml-4'>
           <NavigationMenuItem>
             <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
             <NavigationMenuContent>
-              <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                <li className="row-span-3">
+              <ul className='grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]'>
+                <li className='row-span-3'>
                   <NavigationMenuLink asChild>
                     <a
-                      className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                      href="/"
-                    >
-                      {/* <Icons.logo className='h-6 w-6' /> */}
-                      <div className="mb-2 mt-4 text-lg font-medium">
-                        Tune Stats
-                      </div>
-                      <p className="text-sm leading-tight text-muted-foreground">
+                      className='flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md'
+                      href='/'>
+                      <Image
+                        src={'/tune_stats_logo.png'}
+                        alt='TuneStats Logo'
+                        height={64}
+                        width={64}
+                      />
+                      <div className='mb-2 text-lg font-medium'>Tune Stats</div>
+                      <p className='text-sm leading-tight text-muted-foreground'>
                         Unlock Your Sound. Track. Discover. Repeat.
                       </p>
                     </a>
                   </NavigationMenuLink>
                 </li>
-                <ListItem href="/docs" title="Introduction">
+                <ListItem href='/docs' title='Introduction'>
                   Enhance your music journey with personalized recommendations.
                 </ListItem>
-                <ListItem href="/docs/installation" title="AI Recomendation">
+                <ListItem href='/docs/installation' title='AI Recomendation'>
                   Get personalized music suggestions based on your unique taste
                   and listening habits.
                 </ListItem>
-                <ListItem href="/docs/primitives/typography" title="Stats">
+                <ListItem href='/docs/primitives/typography' title='Stats'>
                   Track your listening trends, top artists, and favorite genres
                   over time.
                 </ListItem>
@@ -114,38 +115,21 @@ export function Navbar() {
           <NavigationMenuItem>
             <NavigationMenuTrigger>Features</NavigationMenuTrigger>
             <NavigationMenuContent>
-              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
-                {components.map((component) =>
-                  component.pro ? (
-                    <div
-                      key={component.title}
-                      className="flex flex-col gap-2 items-start hover:bg-blue-50 p-2 rounded-lg transition-all"
-                    >
-                      <ListItem href={component.href}>
-                        <div className="flex items-center">
-                          <span className="">{component.title}</span>
-                          <span className="text-xs bg-blue-500 text-white px-2 py-1 border border-blue-300 rounded-lg transition-all hover:bg-blue-400 ml-2">
-                            PRO
-                          </span>
-                        </div>
-                      </ListItem>
-                      <div className="text-sm">{component.description}</div>
-                    </div>
-                  ) : (
-                    <ListItem
-                      key={component.title}
-                      title={component.title}
-                      href={component.href}
-                    >
-                      {component.description}
-                    </ListItem>
-                  )
-                )}
+              <ul className='grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]'>
+                {components.map((component) => (
+                  <ListItem
+                    key={component.title}
+                    title={component.title}
+                    pro={component.pro}
+                    href={component.href}>
+                    {component.description}
+                  </ListItem>
+                ))}
               </ul>
             </NavigationMenuContent>
           </NavigationMenuItem>
           <NavigationMenuItem>
-            <Link href="/docs" legacyBehavior passHref>
+            <Link href='/docs' legacyBehavior passHref>
               <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                 Documentation
               </NavigationMenuLink>
@@ -158,22 +142,32 @@ export function Navbar() {
 }
 
 const ListItem = React.forwardRef<
-  React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, ...props }, ref) => {
+  React.ElementRef<'a'>,
+  React.ComponentPropsWithoutRef<'a'> & { pro?: boolean }
+>(({ className, title, children, pro, ...props }, ref) => {
+  const [hoverToggle, setHoverToggle] = useState(false);
   return (
     <li>
       <NavigationMenuLink asChild>
         <a
+          onMouseEnter={() => setHoverToggle(true)}
+          onMouseLeave={() => setHoverToggle(false)}
           ref={ref}
           className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
             className
           )}
-          {...props}
-        >
-          <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+          {...props}>
+          <div className='flex items-center text-sm font-medium leading-none'>
+            {title}
+            {pro && (
+              <span
+                className={`ml-1 px-2 py-0.5 text-xs font-semibold border-2 rounded-lg ${hoverToggle ? 'bg-indigo-600 border-indigo-600' : 'bg-indigo-800 border-indigo-600'} transition-colors duration-350`}>
+                PRO
+              </span>
+            )}
+          </div>
+          <p className='line-clamp-2 text-sm leading-snug text-muted-foreground'>
             {children}
           </p>
         </a>
@@ -181,4 +175,4 @@ const ListItem = React.forwardRef<
     </li>
   );
 });
-ListItem.displayName = "ListItem";
+ListItem.displayName = 'ListItem';
