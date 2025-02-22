2b3f436514e1f7934f88f1d2484b52d75b8b5cfe
itork <itork555@gmail.com>
2025-02-22 23:19:27 +0100

    feat: add Spotify login button and update Navbar layout

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,9 @@
 import { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+import { FaSpotify } from 'react-icons/fa';
+import { supabase } from '../lib/supabase';
 
 import { cn } from '@/lib/utils';
 import {
@@ -55,7 +58,7 @@ const components: {
     href: '/docs/primitives/tabs',
     description:
       'Relive your month in music—your top tracks and artists all in one place!',
-    pro: false,
+    pro: true,
   },
   {
     title: 'AI Recomendation',
@@ -66,77 +69,101 @@ const components: {
   },
 ];
 
+async function signInWithSpotify() {
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: 'spotify',
+  });
+}
+
+const loginWithSpotify = async () => {
+  signInWithSpotify();
+};
+
 export function Navbar() {
   return (
-    <nav className='fixed top-0 left-0 right-0 bg-background z-50 flex items-center px-4 py-2'>
-      <div className='flex items-center space-x-4'>
-        <Link href='/'>
-          <Image src='/tune_stats_logo.png' alt='Logo' width={40} height={40} />
-        </Link>
-      </div>
-      <NavigationMenu>
-        <NavigationMenuList className='ml-4'>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <ul className='grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]'>
-                <li className='row-span-3'>
-                  <NavigationMenuLink asChild>
-                    <a
-                      className='flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md'
-                      href='/'>
-                      <Image
-                        src={'/tune_stats_logo.png'}
-                        alt='TuneStats Logo'
-                        height={64}
-                        width={64}
-                      />
-                      <div className='mb-2 text-lg font-medium'>Tune Stats</div>
-                      <p className='text-sm leading-tight text-muted-foreground'>
-                        Unlock Your Sound. Track. Discover. Repeat.
-                      </p>
-                    </a>
-                  </NavigationMenuLink>
-                </li>
-                <ListItem href='/docs' title='Introduction'>
-                  Enhance your music journey with personalized recommendations.
-                </ListItem>
-                <ListItem href='/docs/installation' title='AI Recomendation'>
-                  Get personalized music suggestions based on your unique taste
-                  and listening habits.
-                </ListItem>
-                <ListItem href='/docs/primitives/typography' title='Stats'>
-                  Track your listening trends, top artists, and favorite genres
-                  over time.
-                </ListItem>
-              </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Features</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <ul className='grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]'>
-                {components.map((component) => (
-                  <ListItem
-                    key={component.title}
-                    title={component.title}
-                    pro={component.pro}
-                    href={component.href}>
-                    {component.description}
+    <nav className='fixed top-0 left-0 right-0 bg-background z-50 flex items-center justify-between px-4 py-2'>
+      <div className='flex items-center'>
+        <div className='flex items-center space-x-4'>
+          <Link href='/'>
+            <Image
+              src='/tune_stats_logo.png'
+              alt='Logo'
+              width={40}
+              height={40}
+            />
+          </Link>
+        </div>
+        <NavigationMenu>
+          <NavigationMenuList className='ml-4'>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <ul className='grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]'>
+                  <li className='row-span-3'>
+                    <NavigationMenuLink asChild>
+                      <a
+                        className='flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md'
+                        href='/'>
+                        <Image
+                          src={'/tune_stats_logo.png'}
+                          alt='TuneStats Logo'
+                          height={64}
+                          width={64}
+                        />
+                        <div className='text-lg font-medium'>Tune Stats</div>
+                        <p className='text-sm leading-tight text-muted-foreground'>
+                          Unlock Your Sound. Track. Discover. Repeat.
+                        </p>
+                      </a>
+                    </NavigationMenuLink>
+                  </li>
+                  <ListItem href='/docs' title='Introduction'>
+                    Enhance your music journey with personalized
+                    recommendations.
                   </ListItem>
-                ))}
-              </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-          <NavigationMenuItem>
-            <Link href='/docs' legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Documentation
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+                  <ListItem href='/docs/installation' title='AI Recomendation'>
+                    Get personalized music suggestions based on your unique
+                    taste and listening habits.
+                  </ListItem>
+                  <ListItem href='/docs/primitives/typography' title='Stats'>
+                    Track your listening trends, top artists, and favorite
+                    genres over time.
+                  </ListItem>
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger>Features</NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <ul className='grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]'>
+                  {components.map((component) => (
+                    <ListItem
+                      key={component.title}
+                      title={component.title}
+                      pro={component.pro}
+                      href={component.href}>
+                      {component.description}
+                    </ListItem>
+                  ))}
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link href='/docs' legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Pricing
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+      <div>
+        <Button onClick={() => loginWithSpotify()}>
+          Log with&nbsp;
+          <FaSpotify className='text-2xl' />
+        </Button>
+      </div>
     </nav>
   );
 }
