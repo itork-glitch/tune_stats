a7c45b507f8adebc0d1c5b60a9816311e7ba0eac
itork <itork555@gmail.com>
2025-03-18 17:50:57 +0100

    feat: add Area Chart component to Playground for visualizing visitor trends

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -30,6 +30,13 @@ const chartData = [
   { month: 'June', desktop: 214 },
 ];
 
+const chartConfig = {
+  desktop: {
+    label: 'Desktop',
+    color: 'hsl(var(--chart-1))',
+  },
+} satisfies ChartConfig;
+
 const Playground = () => {
   const router = useRouter();
   const [session, setSession] = useState<Session | null>(null);
@@ -118,6 +125,59 @@ const Playground = () => {
         )}
 
         <div>lenght: {totalPlaytime}</div>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>Area Chart</CardTitle>
+            <CardDescription>
+              Showing total visitors for the last 6 months
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <ChartContainer config={chartConfig}>
+              <AreaChart
+                accessibilityLayer
+                data={chartData}
+                margin={{
+                  left: 12,
+                  right: 12,
+                }}>
+                <CartesianGrid vertical={false} />
+                <XAxis
+                  dataKey='month'
+                  tickLine={false}
+                  axisLine={false}
+                  tickMargin={8}
+                  tickFormatter={(value) => value.slice(0, 3)}
+                />
+                <ChartTooltip
+                  cursor={false}
+                  content={<ChartTooltipContent indicator='line' />}
+                />
+                <Area
+                  dataKey='desktop'
+                  type='natural'
+                  fill='var(--color-desktop)'
+                  fillOpacity={0.4}
+                  stroke='var(--color-desktop)'
+                />
+              </AreaChart>
+            </ChartContainer>
+          </CardContent>
+          <CardFooter>
+            <div className='flex w-full items-start gap-2 text-sm'>
+              <div className='grid gap-2'>
+                <div className='flex items-center gap-2 font-medium leading-none'>
+                  Trending up by 5.2% this month{' '}
+                  <TrendingUp className='h-4 w-4' />
+                </div>
+                <div className='flex items-center gap-2 leading-none text-muted-foreground'>
+                  January - June 2024
+                </div>
+              </div>
+            </div>
+          </CardFooter>
+        </Card>
       </div>
     </div>
   );
